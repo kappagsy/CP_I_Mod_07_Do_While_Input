3597fc1f9d56beae522781b6c92898d8512ba7f6
kappagsy <kappagsy@mail.uc.edu>
2022-10-10 19:47:54 -0400

    initial commit

---
+++ src/CapitalInitial.java
@@ -0,0 +1,45 @@
+public class CapitalInitial
+{
+    public static void main(String[] args)
+    {
+        for(int r=1; r <=2; r++)
+        {
+            for (int c = 1; c<=20; c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int r=1; r <=5; r++)
+        {
+            System.out.print("*");
+            System.out.println();
+        }
+
+        for(int r=1; r <=2; r++)
+        {
+            for (int c = 1; c<=20; c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int r=1; r <=5; r++)
+        {
+            System.out.print("                   *");
+            System.out.println();
+        }
+
+        for(int r=1; r <=2; r++)
+        {
+            for (int c = 1; c<=20; c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
